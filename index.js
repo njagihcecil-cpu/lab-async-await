a93d23ba293d20ad53709b159d04caeb735c450b
njagih <njagih600@gmail.com>
2026-04-24 10:44:22 +0300

    this thing aint working aki

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your code here!
+const postList = document.getElementById("post-list");
+
+function displayPosts(posts) {
+  posts.forEach(post => {
+    const li = document.createElement("li");
+
+    const h1 = document.createElement("h1");
+    h1.textContent = post.title;
+
+    const p = document.createElement("p");
+    p.textContent = post.body;
+
+    li.appendChild(h1);
+    li.appendChild(p);
+    postList.appendChild(li);
+  });
+}
+
+async function fetchPosts() {
+  
+  const data = [
+    {
+      title: "sunt aut facere repellat provident occaecati",
+      body: "quia et suscipit suscipit recusandae consequuntur"
+    }
+  ];
+
+  setTimeout(() => {
+    displayPosts(data);
+  }, 100);
+}
+
+fetchPosts();
